239ecf1ae246e3112c52801d916ec1955894db99
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2024-12-04 11:31:03 +0100

    Adjust day 4 solution.

--- src/main/scala/day4/Puzzle2.scala
+++ src/main/scala/day4/Puzzle2.scala
@@ -17,7 +17,8 @@ private def getXAreas(wordSearch: WordSearch): List[WordSearch] = {
 private def isXmas(xArea: WordSearch) = {
   val diagonal1 = s"${xArea(0)(0)}${xArea(1)(1)}${xArea(2)(2)}"
   val diagonal2 = s"${xArea(2)(0)}${xArea(1)(1)}${xArea(0)(2)}"
-  List(diagonal1, diagonal2).forall { diagonal =>
-    List(diagonal, diagonal.reverse).contains("MAS")
-  }
+  isDiagonalMas(diagonal1) && isDiagonalMas(diagonal2)
 }
+
+private def isDiagonalMas(diagonal: String) =
+  List(diagonal, diagonal.reverse).contains("MAS")
