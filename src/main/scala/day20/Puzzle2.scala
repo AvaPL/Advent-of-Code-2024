78fe99eee06894cbb058eee87021af4c75502e40
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2024-12-23 22:00:16 +0100

    Add day 20 part 2 solution.

--- src/main/scala/day20/Puzzle2.scala
+++ src/main/scala/day20/Puzzle2.scala
@@ -1,9 +1,9 @@
 package io.github.avapl
 package day20
 
-@main def puzzle1(): Unit = {
+@main def puzzle2(): Unit = {
   val (racetrack, start, end) = PuzzleInputParser.parsedInput
   val positionToTime = measureTime(racetrack, start, end)
-  val result = countCheats(positionToTime, cheatLength = 2, minimumCheatTimeGain = 100)
+  val result = countCheats(positionToTime, cheatLength = 20, minimumCheatTimeGain = 100)
   println(result)
 }
--- src/main/scala/day20/package.scala
+++ src/main/scala/day20/package.scala
@@ -1,6 +1,8 @@
 package io.github.avapl
 package day20
 
+import scala.collection.mutable
+
 enum RacetrackElement {
   case Wall, FreeSpace
 }
@@ -11,17 +13,51 @@ case class Position(row: Int, column: Int) {
 
   lazy val adjacentPositions: List[Position] =
     List(
-      positionAbove,
-      positionBelow,
-      positionToLeft,
-      positionToRight
+      Position(row - 1, column),
+      Position(row + 1, column),
+      Position(row, column - 1),
+      Position(row, column + 1)
     )
-
-  lazy val positionAbove: Position = Position(row - 1, column)
-  lazy val positionBelow: Position = Position(row + 1, column)
-  lazy val positionToLeft: Position = Position(row, column - 1)
-  lazy val positionToRight: Position = Position(row, column + 1)
 }
 
 type Start = Position
 type End = Position
+
+def measureTime(racetrack: Racetrack, start: Start, end: End) = {
+
+  def racetrackElementAt(position: Position): Option[RacetrackElement] =
+    racetrack.lift(position.row).flatMap(_.lift(position.column))
+
+  val positionToTime = mutable.Map.empty[Position, Int]
+  var currentPosition = Option(start)
+  var currentTime = 0
+
+  while (!positionToTime.contains(end)) {
+    positionToTime(currentPosition.get) = currentTime
+    currentPosition = currentPosition.get.adjacentPositions
+      .filterNot(positionToTime.contains)
+      .find(racetrackElementAt(_).contains(RacetrackElement.FreeSpace))
+    currentTime += 1
+  }
+
+  positionToTime.toMap
+}
+
+def countCheats(
+    positionToTime: Map[Position, Int],
+    cheatLength: Int,
+    minimumCheatTimeGain: Int
+) =
+  positionToTime.keys.toList.flatMap { currentPosition =>
+    for {
+      rowOffset <- -cheatLength to cheatLength
+      rowOffsetLength = math.abs(rowOffset)
+      columnOffset <- -cheatLength + rowOffsetLength to cheatLength - rowOffsetLength
+      columnOffsetLength = math.abs(columnOffset)
+      cheatPosition = Position(currentPosition.row + rowOffset, currentPosition.column + columnOffset)
+      if positionToTime.contains(cheatPosition)
+      cheatLength = rowOffsetLength + columnOffsetLength
+      cheatGain = positionToTime(cheatPosition) - positionToTime(currentPosition) - cheatLength
+      if cheatGain >= minimumCheatTimeGain
+    } yield cheatGain
+  }.size
